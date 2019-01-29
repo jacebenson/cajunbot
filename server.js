6d8543e905c40ddc256ea019d427193f5730dd71
Glitch (discord-cajon-bot) <none>
2019-01-29 23:40:41 +0000

    Added Site - redid responses

--- server.js
+++ server.js
@@ -1,17 +1,6 @@
-var express = require("express")
-var keepalive = require('express-glitch-keepalive');
-var app = express();
 var fs = require('fs');
-app.use(keepalive);
-app.get('/', (req, res) => {
-    res.json('Ok');
-});
-var phrase = '!help';
 var Eris = require('eris');
 var bot = new Eris(process.env.DISCORD_BOT_TOKEN);
-bot.on('ready', () => { // When the bot is ready
-    console.log(phrase + ' Ready!'); // Log "Ready!"
-});
 var responses = {};
 var testFolder = './responses/';
 fs.readdir(testFolder, function (err, files) {
@@ -27,4 +16,5 @@ fs.readdir(testFolder, function (err, files) {
   responses['help.js'] = require('./help.js');
   responses['help.js'].command(bot, responses);
   bot.connect(); // Get the bot to connect to Discord
-});//
+});
+var site =  require('./site.js');
