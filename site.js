0d55cb92020d04c49e3fb5e89ed6078685984206
Glitch (discord-cajon-bot) <none>
2019-02-13 02:01:19 +0000

    🌇🚞 Updated with Glitch

--- site.js
+++ site.js
@@ -1,4 +1,4 @@
-module.exports = (function(){
+module.exports = (function () {
   var MongoClient = require('mongodb').MongoClient;
   var mongoURI = process.env.MONGOLAB_URI; // || require('./.env').uri;
   var express = require("express");
@@ -7,21 +7,24 @@ module.exports = (function(){
   app.get("/", function (request, response) {
     response.sendFile(__dirname + '/public/index.html');
   });
+  app.get("/install", function (request, response) {
+    response.redirect('https://discordapp.com/oauth2/authorize?&client_id='+process.env.DISCORD_CLIENT_ID+'&scope=bot&permissions=0');
+  });
   app.get("/scores", function (request, response) {
-    MongoClient.connect(mongoURI, function(err, client) {
-                                console.log('connected to mongo');
-                                if (err) console.log(err);
-                                var db = client.db('cajonbot');
-                                // look for user in db
-                              var queryObj = {};
-                                db.collection('points').find({}).toArray(function(err, result) {
-                                  response.send(result);
-                                });
+    MongoClient.connect(mongoURI, function (err, client) {
+      console.log('connected to mongo');
+      if (err) console.log(err);
+      var db = client.db('cajonbot');
+      // look for user in db
+      var queryObj = {};
+      db.collection('points').find({}).toArray(function (err, result) {
+        response.send(result);
+      });
     });
-//    response.send('....');
+    //    response.send('....');
   });
   var listener = app.listen(process.env.PORT, function () {
     console.log('Your app is listening on port ' + listener.address().port);
   });
-  
+
 })();
