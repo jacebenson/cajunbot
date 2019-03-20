967503a424fc291fcdba46f1ab51ecd86d125862
Jace <jace.benson@protonmail.com>
2019-03-20 15:49:22 -0500

    Update site.js

--- site.js
+++ site.js
@@ -1,24 +1,24 @@
 module.exports = (function () {
-  var port = process.env.PORT || 8000;
+  var port = process.env.PORT || 80;
   var translate = require('moji-translate');
   var MongoClient = require('mongodb').MongoClient;
   var mongoURI = process.env.MONGODB_URI; // || require('./.env').uri;
   var express = require("express");
   var app = express();
   app.use(express.static('public')); //use static files in ROOT/public folder
-  app.get("/", function (request, response) {
+  app.get("/discord/leaderboard", function (request, response) {
     response.sendFile(__dirname + '/public/index.html');
   });
-  app.get("/install", function (request, response) {
+  app.get("/discord/install", function (request, response) {
     response.redirect('https://discordapp.com/oauth2/authorize?&client_id=' + process.env.DISCORD_CLIENT_ID + '&scope=bot&permissions=0');
   });
-  app.get("/emoji", function (request, response) {
+  app.get("/discord/emoji", function (request, response) {
     console.log(request.query.text);
     response.set('Content-Type', 'application/json');
     response.send(JSON.stringify({text:translate.translate(request.query.text)}));
     //response.redirect('https://discordapp.com/oauth2/authorize?&client_id=' + process.env.DISCORD_CLIENT_ID + '&scope=bot&permissions=0');
   });
-  app.get("/scores", function (request, response) {
+  app.get("/discord/scores", function (request, response) {
     MongoClient.connect(mongoURI, function (err, client) {
       console.log('connected to mongo');
       if (err) console.log(err);
