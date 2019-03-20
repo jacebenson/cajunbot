0601c52031ced8ef858476a3b1d39a8c2cba0574
Jace <jace.benson@protonmail.com>
2019-03-20 16:34:54 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 require('dotenv').config();
 var fs = require('fs');
 var Eris = require('eris');
-require('./site.js');
+'use strict';
+
+require('greenlock-express').create({
+  email: 'jace.benson@protonmail.com'     // The email address of the ACME user / hosting provider
+, agreeTos: true                    // You must accept the ToS as the host which handles the certs
+, configDir: '~/.config/acme/'      // Writable directory where certs will be saved
+, communityMember: true             // Join the community to get notified of important updates
+, telemetry: true                   // Contribute telemetry data to the project
+
+  // Using your express app:
+  // simply export it as-is, then include it here
+, app: require('./site.js')
+
+//, debug: true
+}).listen(80, 443);
 //require('./integrations/sndevs.slack').connect();//disabled after token was revoked
 var bot = new Eris(process.env.DISCORD_BOT_TOKEN);
 var responses = {};
