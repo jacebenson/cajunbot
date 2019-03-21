6f95c425b6b06d88557e151b610558c0ad901382
Jace Benson <jace.benson@protonmail.com>
2019-03-21 01:39:04 -0500

    adding slackbot responses

---
+++ responses/slackbot.js
@@ -0,0 +1,36 @@
+module.exports = {
+    command: function (bot, msg) {
+        var phrases = [
+            {'dom manipulation':["(－‸ლ)","ಠ_ಠ"]},
+            {'global business rule':["(－‸ლ)","ಠ_ಠ"]},
+            {'global br':["(－‸ლ)","ಠ_ಠ"]},
+            {'window.':["ಠ_ಠ"]},
+            {'top.':["ಠ_ಠ"]},
+            {'document.':["ಠ_ಠ"]},
+            {'facepalm':["(－‸ლ)"]},
+            {'(╯°□°）╯︵ ┻━┻':["┬──┬ ノ( ゜-゜ノ)"]},
+            {'well it works':["http://i.imgur.com/vSaxB.jpg"]},
+            {'current.update':["(－‸ლ)","ಠ_ಠ"]},
+            {'global ui script':["(－‸ლ)","ಠ_ಠ"]},
+            {'!xplore':["https://github.com/thewhitespace/Xplore/tree/master/dist"]},
+        ];
+        if (msg.author.bot === false) {
+            var wordsArr = msg.content.split(' ');
+            wordsArr.map(function(word, index) {
+                phrases.map(function(phrase) {
+                    if(typeof phrase[word.toLowerCase()]){
+                        var message = rand(phrase[word.toLowerCase()]);
+                        bot.createMessage(msg.channel.id, message);
+                    }
+                });
+            });
+        }
+    },
+    help: 'Some strings have custom responses (see slackbot.js)'
+};
+
+var rand = function(arr) {
+    //console.log('in random.');
+    var random_choice = Math.floor(Math.random() * arr.length);
+    return arr[random_choice];
+}
