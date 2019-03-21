2a631126561f5399a72e6d60be2ce715e86632b4
Jace Benson <jace.benson@protonmail.com>
2019-03-21 01:46:32 -0500

    ......

--- responses/slackbot.js
+++ responses/slackbot.js
@@ -1,30 +1,28 @@
 module.exports = {
     command: function (bot, msg) {
         try{
-        var phrases = [
-            {'dom manipulation':["(－‸ლ)","ಠ_ಠ"]},
-            {'global business rule':["(－‸ლ)","ಠ_ಠ"]},
-            {'global br':["(－‸ლ)","ಠ_ಠ"]},
-            {'window.':["ಠ_ಠ"]},
-            {'top.':["ಠ_ಠ"]},
-            {'document.':["ಠ_ಠ"]},
-            {'facepalm':["(－‸ლ)"]},
-            {'(╯°□°）╯︵ ┻━┻':["┬──┬ ノ( ゜-゜ノ)"]},
-            {'well it works':["http://i.imgur.com/vSaxB.jpg"]},
-            {'current.update':["(－‸ლ)","ಠ_ಠ"]},
-            {'global ui script':["(－‸ლ)","ಠ_ಠ"]},
-            {'!xplore':["https://github.com/thewhitespace/Xplore/tree/master/dist"]},
-        ];
+        var phrases = {
+            'dom manipulation':["(－‸ლ)","ಠ_ಠ"],
+            'global business rule':["(－‸ლ)","ಠ_ಠ"],
+            'global br':["(－‸ლ)","ಠ_ಠ"],
+            'window.':["ಠ_ಠ"],
+            'top.':["ಠ_ಠ"],
+            'document.':["ಠ_ಠ"],
+            'facepalm':["(－‸ლ)"],
+            '(╯°□°）╯︵ ┻━┻':["┬──┬ ノ( ゜-゜ノ)"],
+            'well it works':["http://i.imgur.com/vSaxB.jpg"],
+            'current.update':["(－‸ლ)","ಠ_ಠ"],
+            'global ui script':["(－‸ლ)","ಠ_ಠ"],
+            '!xplore':["https://github.com/thewhitespace/Xplore/tree/master/dist"],
+        };
         if (msg.author.bot === false) {
             var wordsArr = msg.content.split(' ');
             wordsArr.map(function(word, index) {
-                phrases.map(function(phrase) {
-                    if(typeof phrase[word.toLowerCase()]){
-                        console.log('phrase['+word.toLowerCase()+']:' + phrase[word.toLowerCase()])
-                        var message = rand(phrase[word.toLowerCase()]);
-                        bot.createMessage(msg.channel.id, message);
-                    }
-                });
+                if(typeof phrases[word.toLowerCase()]){
+                    console.log('phrases['+word.toLowerCase()+']:' + phrases[word.toLowerCase()])
+                    var message = rand(phrases[word.toLowerCase()]);
+                    bot.createMessage(msg.channel.id, message);
+                }
             });
         }
     }catch(e){
