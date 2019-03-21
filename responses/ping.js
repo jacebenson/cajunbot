4b8067a102e9cacd6f77044f545f574ea8ec09d6
Jace <jace.benson@protonmail.com>
2019-03-21 15:15:55 -0500

    Update ping.js

--- responses/ping.js
+++ responses/ping.js
@@ -15,13 +15,13 @@ module.exports = {
                     });
                 bot.deleteMessage(msg.channel.id, msg.id, 'Test Delete Content');
                 */
-                    const https = require('https');
+                    var https = require('https');
                     var url = msg.content.replace(phrase, '').trim();
                     if(url.indexOf('http')==-1){
                     url = 'https://' + url;
                     }
                     https.get(url, (resp) => {
-                        let data = '';
+                        var data = '';
 
                         // A chunk of data has been recieved.
                         resp.on('data', (chunk) => {
@@ -30,7 +30,7 @@ module.exports = {
 
                         // The whole response has been received. Print out the result.
                         resp.on('end', () => {
-                            console.log(JSON.parse(data).explanation);
+                            //console.log(JSON.parse(data).explanation);
                             bot.createMessage(msg.channel.id, resp.statusCode);
                         });
 
