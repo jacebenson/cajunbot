9c9dee0e9d26c7a05ce6fdb69920f9eafa0be565
Jace <jace.benson@protonmail.com>
2019-03-21 15:24:03 -0500

    Update ping.js

--- responses/ping.js
+++ responses/ping.js
@@ -5,35 +5,22 @@ module.exports = {
             var wordsArr = msg.content.split(' ');
             wordsArr.map(function(word, index) {
                 if (word.toLowerCase() === phrase) {
-                    //msg.addReaction('💯'); //now geting error unknown emoji
-                    //msg.addReaction('😅');
-                    /*var msgs = bot.getMessages(msg.channel.id, 100).then(function(messages){
-                      messages.map(function(message){
-                        console.log('Deleting "'+message.content+'"');
-                      bot.deleteMessage(msg.channel.id, message.id, 'Test Delete Content');
-                      });
-                    });
-                bot.deleteMessage(msg.channel.id, msg.id, 'Test Delete Content');
-                */
                     var https = require('https');
                     var url = msg.content.replace(phrase, '').trim();
                     if(url.indexOf('http')==-1){
-                    url = 'https://' + url;
+                        url = 'https://' + url;
                     }
                     https.get(url, (resp) => {
                         var data = '';
-
                         // A chunk of data has been recieved.
                         resp.on('data', (chunk) => {
                             data += chunk;
                         });
-
                         // The whole response has been received. Print out the result.
                         resp.on('end', () => {
                             //console.log(JSON.parse(data).explanation);
                             bot.createMessage(msg.channel.id, resp.statusCode);
                         });
-
                     }).on("error", (err) => {
                         console.log("Error: " + err.message);
                     });
