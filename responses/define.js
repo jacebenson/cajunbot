19e129d62350de850b211c6aedfd56a9a9c33ea9
Jace Benson <jace.benson@protonmail.com>
2019-04-05 22:36:34 -0500

    just getting top result

--- responses/define.js
+++ responses/define.js
@@ -10,13 +10,10 @@ module.exports = {
                         var search = wordsArr.join('').replace(word, '');
                         ud.term(search).then((result) => {
                             var entries = result.entries
-                            var o = rand([1,2,3,4,5,6,7,8,9,0]);
-                            console.log('going to use: ' + o);
-                            console.log('should get: ' + entries[o].word);
                             if(entries.length>0){
                             //console.log(entries[0].word)
                             //console.log(winner[0].definition)
-                            bot.createMessage(msg.channel.id, entries[o].definition);    
+                            bot.createMessage(msg.channel.id, entries[0].definition);    
                             //console.log(entries[0].example)
                             } else {
                                 bot.createMessage(msg.channel.id, "Define it: " + 'https://www.urbandictionary.com/add.php?word=' + search);
