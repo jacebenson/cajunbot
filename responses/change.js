0a06d2765cde37fdc2cdb67bd8224053f1c0760f
Jace Benson <jacebenson@fico.com>
2019-04-06 10:27:22 -0500

    added chance

---
+++ responses/change.js
@@ -0,0 +1,36 @@
+var chance = require('chance');
+module.exports = {
+    command: function (bot, msg) {
+        try {
+            if (msg.author.bot === false) {
+                var allowedDice = {
+                    "4": true,
+                    "6": true,
+                    "8": true,
+                    "10": true,
+                    "12": true,
+                    "20": true,
+                    "30": true,
+                    "100": true
+                };
+                wordsArr = msg.content.toLowerCase().split(' ');
+                wordsArr.forEach(function (word) {
+                    if (word.indexOf('d') > -1) {
+                        //try dice
+                        var dice = word.split('d');
+                        var num = parseInt(dice[0]);
+                        if (isNaN(num) === false) {
+                            if (allowedDice[dice[1]]) {
+                                msg.channel.send(message);
+                                chance.rpg(word);
+                            }
+                        }
+                    }
+                });
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    },
+    help: '#d4,6,8,10,12,20,30,100 roll dice'
+};
