fee19fac807f2be10a21e4aa7674ef6e47356b00
Jace Benson <jace.benson@protonmail.com>
2019-04-05 22:37:41 -0500

    reverting

--- responses/define.js
+++ responses/define.js
@@ -10,17 +10,13 @@ module.exports = {
                         var search = wordsArr.join('').replace(word, '');
                         ud.term(search).then((result) => {
                             var entries = result.entries
-                            if(entries.length>0){
-                            //console.log(entries[0].word)
-                            //console.log(winner[0].definition)
+                            console.log(entries[0].word)
+                            console.log(entries[0].definition)
                             bot.createMessage(msg.channel.id, entries[0].definition);    
-                            //console.log(entries[0].example)
-                            } else {
-                                bot.createMessage(msg.channel.id, "Define it: " + 'https://www.urbandictionary.com/add.php?word=' + search);
-                            }
+                            console.log(entries[0].example)
                           }).catch((error) => {
                             console.error(error.message)
-                          });
+                          })
                     }
                 });
             });
