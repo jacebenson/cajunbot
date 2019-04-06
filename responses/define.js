7e8103c35806c6c69049332b503268d30cd52ecf
Jace Benson <jace.benson@protonmail.com>
2019-04-05 21:45:54 -0500

    add define command

---
+++ responses/define.js
@@ -0,0 +1,52 @@
+var https = require('https');
+var URL = require('url').URL;
+module.exports = {
+    command: function(bot, msg) {
+        var phrases = ['!define', '!def'];
+        if (msg.author.bot === false) {
+            var wordsArr = msg.content.split(' ');
+            wordsArr.map(function(word, index) {
+                phrases.map(function(phrase) {
+                    if (word.toLowerCase() === phrase) {
+                        var url = 'https://api.urbandictionary.com/v0/define?term=' + wordsArr.replace(phrase, '');
+                        console.log(url);
+                        url = new URL(url);
+                        var options = {
+                            timeout: 3000,
+                            host: url.host,
+                            path: url.pathname,
+                            headers: {
+                                'Accept': 'application/json'
+                            }
+                        }
+                        https.get(options, (resp) => {
+                            var data = '';
+                            resp.on('data', (chunk) => {
+                                data += chunk;
+                            });
+                            // The whole response has been received. Print out the result.
+                            resp.on('end', () => {
+                                //console.log(JSON.parse(data).explanation);
+                                var message;
+                                var obj = JSON.parse(data);
+                                if(obj.joke){
+                                    message = obj[0].definition;
+                                }
+                                bot.createMessage(msg.channel.id, message);
+                            });
+                        }).on("error", (err) => {
+                            bot.createMessage(msg.channel.id, "Error: `" + err.message + "`");
+                        });
+                    }
+                });
+            });
+        }
+    },
+    help: '`!joke` Will get a joke from one of two apis.'
+};
+
+var rand = function(arr) {
+    //console.log('in random.');
+    var random_choice = Math.floor(Math.random() * arr.length);
+    return arr[random_choice];
+}
