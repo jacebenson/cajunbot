1624208c069b78c1e9918d5136a0f4cf5947bfcd
Jace <jace.benson@protonmail.com>
2019-04-10 22:22:09 -0500

    Update job.js

--- responses/job.js
+++ responses/job.js
@@ -1,3 +1,6 @@
+var Chance = require('chance');
+// Instantiate Chance so it can be used
+var chance = new Chance();
 module.exports = {
     command: function(bot, msg) {
         var phrase = '!job';
@@ -9,64 +12,73 @@ module.exports = {
                     var message = [];
                     var parts = {};
                     parts.clown = {};
-                    parts.clown.name = [
-                        '${recruiter_name}',
-                        'Christine',
-                        'Jane',
-                        'Kimberly',
-                        'Matt',
-                        'Todd',
-                    ];
-                    parts.clown.signature = [
-                        'Best,',
-                        'Cheers,',
-                        'Have a good one!',
-                        'Regards,'
-                    ];
                     parts.dip = {};
-                    parts.dip.day = [
+                    parts.dip.day = chance.pickone([
                         'hows',
                         'would you have time on',
-                    ];
-                    parts.dip.suggest = [
+                    ]);
+                    parts.dip.suggest = chance.pickone([
                         'How about we',
                         'Lets'
-                    ];
-                    parts.dip.time = [
-                        'Monday',
-                        'Tuesday',
-                        'Wednesday',
-                        'Thursday',
-                        'Friday',
-                    ];
-                    parts.dip.to_chat = [
+                    ]);
+                    parts.dip.to_chat = chance.pickone([
                         'and talk further',
                         'and chat some more',
                         'to discuss'
-                    ];
-                    parts.dip.way = [
+                    ]);
+                    parts.dip.way = chance.pickone([
                         'hop on the phone',
                         'grab coffee'
-                    ];
+                    ]);
                     parts.flatter = {};
-                    parts.flatter.role = [
+                    parts.flatter.role = chance.pickone([
                         'Architect',
                         'Lead Developer',
                         'System Administrator',
                         '${job_title}',
                         'Implementation Specialist',
-                        'Senior Developer over the other Senior Developer'
-                    ];
-                    parts.flatter.skill_one = [
+                        'Senior Developer over the other Senior Developer',
+                        'Developer, full-stack',
+                        'Developer, back-end',
+                        'Developer, front-end',
+                        'Developer, desktop or enterprise applications',
+                        'Developer, mobile',
+                        'Database administrator',
+                        'Designer',
+                        'System administrator',
+                        'DevOps specialist',
+                        'Developer, embedded applications or devices',
+                        'Data scientist or machine learning specialist',
+                        'Developer, QA or test',
+                        'Data or business analyst',
+                        'Academic researcher',
+                        'Engineer, data',
+                        'Educator',
+                        'Developer, game or graphics',
+                        'Engineering manager',
+                        'Product manager',
+                        'Scientist',
+                        'Engineer, site reliability',
+                    ]);
+                    parts.flatter.skills = chance.pickset([
                         'jQuery',
                         'HTML',
                         'AJAX',
                         'AngularJS',
                         'JavaScript',
                         'CSS',
-                        'Bootstrap'
-                    ];
-                    parts.flatter.skill_two = [
+                        'Bootstrap',
+                        'Angular/Angular.js',
+                        'React.js',
+                        'ASP.NET',
+                        'Express',
+                        'Spring',
+                        'Vue.js',
+                        'Django',
+                        'Flask',
+                        'Laravel',
+                        'Ruby on Rails',
+                        'Drupal',
                         'ITAM',
                         'ITIL',
                         'Agile Development',
@@ -74,56 +86,55 @@ module.exports = {
                         'SecOps',
                         'ITOM',
                         'CSM',
-                        'ITSM'
-                    ];
-                    parts.flatter.start = [
+                        'ITSM'], 2);
+                    parts.flatter.start = chance.pickone([
                         'With your considerable talents in',
                         'Based on your impressive skills with',
                         'Given your extensive experience in',
                         'Amazed by your knowledge of'
-                    ];
-                    parts.flatter.suggest = [
+                    ]);
+                    parts.flatter.suggest = chance.pickone([
                         'we thing you should join our stellar team as its',
                         'I think youd be a great fit for our',
                         'lets discuss your potiental future as our team\'s',
                         'I think youd make an incredible addition to our team as its'
-                    ];
+                    ]);
                     parts.greet = {};
-                    parts.greet.adjective = [
+                    parts.greet.adjective = chance.pickone([
                         'a super',
                         'an awesome',
                         'a fantastic'
-                    ];
-                    parts.greet.day = [
+                    ]);
+                    parts.greet.day = chance.pickone([
                         'weekend!',
-                        'Monday!',
+                        chance.weekday({weekday_only: true}) + '!',
                         'day!'
-                    ];
-                    parts.greet.end = [
+                    ]);
+                    parts.greet.end = chance.pickone([
                         'Super-pumped to meet you!',
                         'So awesome to e-meet you!'
-                    ];
+                    ]);
                     parts.intro = {};
-                    parts.intro.adjective = [
+                    parts.intro.adjective = chance.pickone([
                         'de-centralized',
                         'sharing economy-inspired',
                         'deep learning-powered',
                         'AI-infused',
                         'blockchain-enabled',
                         'scalable'
-                    ];
-                    parts.intro.change = [
+                    ]);
+                    parts.intro.change = chance.pickone([
                         'to re-design the boundaries of',
                         'to design the future of',
                         'to change the world of',
                         'to push the limits of'
-                    ];
-                    parts.intro.company = [
+                    ]);
+                    parts.intro.company = chance.pickone([
                         'company',
                         'startup',
                         'employer'
-                    ];
-                    parts.intro.company_flair = [
+                    ]);
+                    parts.intro.company_flair = chance.pickone([
                         'Atom',
                         'Waste',
                         'Deep',
@@ -131,23 +142,14 @@ module.exports = {
                         'Xyz',
                         'Square',
                         'Garbage'
-                    ];
-                    parts.intro.company_suffix = [
+                    ]);
+                    parts.intro.company_suffix = chance.pickone([
                         'Box',
                         'Cube',
                         '.ai',
                         'able'
-                    ];
-                    parts.intro.example = [
-                        'Pets.com',
-                        'Enron',
-                        'Zenefits',
-                        'Theranos',
-                        'Lehman Brothers',
-                        'Juicero'
-
-                    ];
-                    parts.intro.funds = [
+                    ]);
+                    parts.intro.funds = chance.pickone([
                         'millions',
                         '$600k on KickStarter',
                         '$50M in our series C',
@@ -158,8 +160,8 @@ module.exports = {
                         '4 million garlic coins',
                         '660 $100 Amazon gift cards',
                         '500 Points Thing points'
-                    ];
-                    parts.intro.impact = [
+                    ]);
+                    parts.intro.impact = chance.pickone([
                         'help desks',
                         'scoped apps',
                         'IT for IT',
@@ -168,34 +170,34 @@ module.exports = {
                         'hangover cures',
                         'human resources',
                         'using memes in the workplace'
-                    ];
+                    ]);
 
                     if (Math.random() > 0.5) {
                         message.push('Hope you\'re having');
-                        message.push(rand(parts.greet.adjective));
-                        message.push(rand(parts.greet.day));
+                        message.push(parts.greet.adjective);
+                        message.push(parts.greet.day);
                     }
-                    message.push(rand(parts.greet.end));
+                    message.push(parts.greet.end);
                     message.push('My');
-                    message.push(rand(parts.intro.company));
-                    message.push(rand(parts.intro.company_flair) + rand(parts.intro.company_suffix) + ', a');
-                    message.push(rand(parts.intro.adjective));
-                    message.push(rand(parts.intro.example) + ', has just raised');
-                    message.push(rand(parts.intro.funds));
-                    message.push(rand(parts.intro.change));
-                    message.push(rand(parts.intro.impact) + '.\n\n');
-                    message.push(rand(parts.flatter.start));
-                    message.push(rand(parts.flatter.skill_one) + ' and');
-                    message.push(rand(parts.flatter.skill_two) + ',');
-                    message.push(rand(parts.flatter.suggest));
-                    message.push(rand(parts.flatter.role) + '.');
-                    message.push(rand(parts.dip.suggest));
-                    message.push(rand(parts.dip.way));
-                    message.push(rand(parts.dip.to_chat) + '—');
-                    message.push(rand(parts.dip.day));
-                    message.push(rand(parts.dip.time) + '?\n\n');
-                    message.push(rand(parts.clown.signature) + '\n');
-                    message.push(rand(parts.clown.name));
+                    message.push(parts.intro.company);
+                    message.push(parts.intro.company_flair + parts.intro.company_suffix + ', a');
+                    message.push(parts.intro.adjective);
+                    message.push(chance.company() + ', has just raised');
+                    message.push(parts.intro.funds);
+                    message.push(parts.intro.change);
+                    message.push(parts.intro.impact + '.\n\n');
+                    message.push(parts.flatter.start);
+                    message.push(parts.flatter.skills.join(' and '));
+                    message.push(parts.flatter.skill_two + ',');
+                    message.push(parts.flatter.suggest);
+                    message.push(parts.flatter.role + '.');
+                    message.push(parts.dip.suggest);
+                    message.push(parts.dip.way);
+                    message.push(parts.dip.to_chat + '—');
+                    message.push(parts.dip.day);
+                    message.push(chance.weekday({weekday_only: true}) + '?\n\n');
+                    message.push(parts.clown.signature + '\n');
+                    message.push(chance.first({ nationality: 'us' }));
                     var message = message.join(' ');
                     msg.channel.send(message);
                 }
