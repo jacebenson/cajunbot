c47e3ea62a8cc45b6d50b012a47f8e734fd99399
Jace Benson <jacebenson@fico.com>
2019-04-17 11:20:28 -0500

    ....

--- integrations/timetable.js
+++ integrations/timetable.js
@@ -94,8 +94,8 @@ module.exports = {
         bot.on("message", function(msg) {
             if(msg.author.id === jace){
                 var now = new Date();
-                now = now.setHours(0);
-                now = now.setMinutes(0);
+                now.setHours(0);
+                now.setMinutes(0);
                 var date = {
                     today:      now,
                     t:          now,
