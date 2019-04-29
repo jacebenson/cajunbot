73a30e8d022e1fee59221fe5a1d20e1aae288496
Jace <jace.benson@protonmail.com>
2019-04-28 23:45:06 -0500

    Update joke.js

--- responses/joke.js
+++ responses/joke.js
@@ -29,16 +29,17 @@ module.exports = {
                             // The whole response has been received. Print out the result.
                             resp.on('end', () => {
                                 //console.log(JSON.parse(data).explanation);
-                                var message = '';
                                 var obj = JSON.parse(data);
                                 obj.forEach(jokeEntry => {
+                                    var message = '';
                                     if(jokeEntry.joke && jokeEntry.punchline){
-                                        message += jokeEntry.joke + ' || ' + jokeEntry.punchline + ' || \n';
+                                        message += jokeEntry.joke + ' || ' + jokeEntry.punchline + '||\n';
                                     } else {
                                         message += jokeEntry.joke + '\n';
                                     }
-                                });
+                                    
                                 msg.channel.send(message);
+                                });
                             });
                         }).on("error", (err) => {
                             msg.channel.send("Error: `" + err.message + "`");
