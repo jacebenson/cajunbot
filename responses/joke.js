03a0d0bca63088d82aac955cbc431061cf807bcd
Jace <jace.benson@protonmail.com>
2019-04-28 23:48:33 -0500

    Update joke.js

--- responses/joke.js
+++ responses/joke.js
@@ -9,6 +9,10 @@ module.exports = {
                 phrases.map(function (phrase) {
                     if (word.toLowerCase() === phrase) {
                         var n = wordsArr[index + 1] || 1;
+                        if(n>20){
+                            n = 20;
+                            msg.channel.send("That's too many.  Here's 20.");
+                        }
                         console.log('in !joke with phrase "' + phrase + '" and where n = ' + n);
                         var url = 'https://wizardly-wing-66188a.netlify.com/.netlify/functions/server/many/' + n;
                         console.log(url);
@@ -50,5 +54,5 @@ module.exports = {
             });
         }
     },
-    help: '`!joke` or `joke id=1` or `!joke 10` Will get a joke from one of two apis.'
+    help: '`!joke` or `!joke 10` Will get a dad joke / pun.'
 };
