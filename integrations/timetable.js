e448f2d6efc6158b05348c05edca53ecad9591d7
Jace Benson <jacebenson@fico.com>
2019-05-02 14:36:29 -0500

    ....

--- integrations/timetable.js
+++ integrations/timetable.js
@@ -140,11 +140,7 @@ module.exports = {
                     '!tw-notes' :          {query: { "$and":[{ date: { "$gt": date.thisweek                     } }, { type: "note"}    ] } },
                     '!tw-events':          {query: { "$and":[{ date: { "$gt": date.thisweek                     } }, { type: "event"}   ] } },
                 };
-                phrases['!s'] = (function(wordsArr, index){
-                    return {
-                        query: {query: { "$and":[{ comment : '/.*' +wordsArr[index+1]+ '.*/i'}]}}
-                    }
-                })(wordsArr, index)
+                phrases['!s'] = true;
                 //https://www.petitemelanie.com/en/the-bullet-journal-method-how-it-works/
                 //https://bulletjournal.com/pages/book
                 if (msg.author.bot === false) {
