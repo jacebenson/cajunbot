1a231e6f8a32334869167c3bb13f5007750de519
Jace Benson <jacebenson@fico.com>
2019-05-17 14:22:28 -0500

    fix cat facts

--- responses/cat.js
+++ responses/cat.js
@@ -5,36 +5,34 @@ module.exports = {
             var wordsArr = msg.content.split(' ');
             wordsArr.map(function(word, index) {
                 if (word.toLowerCase() === phrase) {
-                    var url = 'https://catfact.ninja/fact';
-                    var https = require('https');
-                    var URL = require('url').URL;
-                    //var myURL = new URL('http://www.example.com/foo?bar=1#main');
-                    var url = new URL(url);
+                    var http = require("https");
+
                     var options = {
-                        timeout: 3000,
-                        host: url.host,
-                        path: url.pathname,
-                        headers: {
-                            "Accept": "application/json"
-                        }
-                    }
-                    https.get(options, (resp) => {
-                        var data = '';
-                        resp.on('data', (chunk) => {
-                            data += chunk;
-                        });
-                        // The whole response has been received. Print out the result.
-                        resp.on('end', () => {
-                            //console.log(JSON.parse(data).explanation);
-                            var message;
-                            if(resp.statusCode === 200){
-                                message = resp.fact;
-                            }
-                            msg.channel.send(message);
-                        });
-                    }).on("error", (err) => {
-                        msg.channel.send("Error: `" + err.message + "`");
+                      "method": "GET",
+                      "hostname": "catfact.ninja",
+                      "port": null,
+                      "path": "/fact",
+                      "headers": {
+                        "content-length": "0"
+                      }
+                    };
+                    
+                    var req = http.request(options, function (res) {
+                      var chunks = [];
+                    
+                      res.on("data", function (chunk) {
+                        chunks.push(chunk);
+                      });
+                    
+                      res.on("end", function () {
+                        var body = Buffer.concat(chunks);
+                        console.log(body.toString());
+                        var responseObj = JSON.parse(body.toString());
+                        msg.channel.send(responseObj.fact);
+                      });
                     });
+                    
+                    req.end();
                 }
             });
         }
