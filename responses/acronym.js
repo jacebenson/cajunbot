5369a8f2c942f2fb93109c22c2604184c6d9205d
Jace Benson <jace.benson@protonmail.com>
2019-09-24 16:55:46 -0500

    add acroynym

---
+++ responses/acronym.js
@@ -0,0 +1,19 @@
+var acroynm = require('acronym')
+module.exports = {
+    command: function(bot, msg) {
+        var phrases = ['!acronym', '!ac'];
+        if (msg.author.bot === false) {
+            var wordsArr = msg.content.split(' ');
+            wordsArr.map(function(word, index) {
+                phrases.map(function(phrase) {
+                    if (word.toLowerCase() === phrase) {
+                        var search = wordsArr.join('').replace(word, '');
+                        msg.channel.send(acroynm(search));
+                        console.log(entries[0].example)
+                    }
+                });
+            });
+        }
+    },
+    help: '`!acroynm` letters'
+};
