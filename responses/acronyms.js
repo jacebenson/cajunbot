968ecc000fb3dac0b61be7639475a6a70c50ebe1
Jace Benson <jace.benson@protonmail.com>
2019-09-24 17:07:06 -0500

    fixed typo

--- responses/acronyms.js
+++ responses/acronyms.js
@@ -1,13 +1,13 @@
 module.exports = {
     command: function(bot, msg) {
-        var acromyn = require('acronym');
+        var acronym = require('acronym');
         var phrases = ['!ac', '!acroymn'];
         if (msg.author.bot === false) {
             var wordsArr = msg.content.split(' ');
             wordsArr.map(function(word, index) {
                 phrases.map(function(phrase) {
                     if (word.toLowerCase() === phrase) {
-                        var message = acromyn(word[index + 1]);
+                        var message = acronym(word[index + 1]);
                         msg.channel.send(message);
                     }
                 });
