01f6d229ed892960bb16beb7db7c68ca46c2f609
Jace <jace.benson@protonmail.com>
2019-10-01 15:17:39 -0500

    Update acronyms.js

--- responses/acronyms.js
+++ responses/acronyms.js
@@ -7,8 +7,18 @@ module.exports = {
             wordsArr.map(function(word, index) {
                 phrases.map(function(phrase) {
                     if (word.toLowerCase() === phrase) {
-                        var message = acronym(wordsArr[index+1]);
-                        msg.channel.send(message);
+                        var firstWord = wordsArr[index+1];
+                        var message = "";
+                        if(firstWord == "snow"){
+                            message = "Snow is not an acroynm.  It's a word and it's a product, but it isn't ServiceNow."
+                        }
+                        if(firstWord.endsWith("m"){
+                           firstWord = firstWord.substr(0,firstWord.length-1);
+                           message = acronym(firstWord) + " management";
+                        } else {
+                            message = acronym(wordsArr[index+1]);        
+                        }
+                    msg.channel.send(message);
                     }
                 });
             });
