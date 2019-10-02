8800ae8eb6883c31fa0d67bd8e16ea139c593c91
Jace Benson <jace@protonmail.com>
2019-10-02 00:02:30 -0500

    added imgflip to chuck calls

--- responses/chuck-norris.js
+++ responses/chuck-norris.js
@@ -1,42 +1,68 @@
-module.exports = {
-    command: function(bot, msg) {
-//console.log('in cat command');
-        var phrase = '!chuck';
-//console.log('msg.author.bot', msg.author.bot);
-        if (msg.author.bot === false) {
-            var wordsArr = msg.content.split(' ');
-//console.log('wordsArr',wordsArr);
-            wordsArr.map(function(word, index) {
-                if (word.toLowerCase() === phrase) {
-                    var http = require("https");
+module.exports = {
+    command: function (bot, msg) {
+        var phrase = '!chuck';
+        if (msg.author.bot === false) {
+            var wordsArr = msg.content.split(' ');
+            wordsArr.map(function (word, index) {
+                if (word.toLowerCase() === phrase) {
+                    var http = require("https");
+                    var req = http.request({
+                        "method": "GET",
+                        "hostname": "api.chucknorris.io",
+                        "port": null,
+                        "path": "/jokes/random"
+                    }, function (chuckRes) {
+                        var chuckChunks = [];
+                        chuckRes.on("data", function (chunk) {
+                            chuckChunks.push(chunk);
+                        });
+                        chuckRes.on("end", function () {
+                            var chuckNorrisTemplates = [
+                                '38579871',//Chuck-Norris-Guns
+                                '16321730',//Chuck-Norris-Finger
+                                '3411280',//Chuck-Norris-Flex
+                            ];
+                            var template = chuckNorrisTemplates[Math.floor(Math.random()*chuckNorrisTemplates.length)];
+                            var body = Buffer.concat(chuckChunks);
+                            var chuckObj = JSON.parse(body.toString());
+                            console.log('chuckObj', chuckObj);
+                            var path = "/caption_image?";
+                            path += "username=" + process.env.imgflipUSER + "&";
+                            path += "password=" + process.env.imgflipPASS + "&";
+                            path += "template_id=" + template + "&";
+                            path += "text1=" +  encodeURIComponent(chuckObj.value);
+                            console.log(path);
+                            var imgFlip = http.request({
+                                "method": "POST",
+                                "hostname": "api.imgflip.com",
+                                "port": null,
+                                "path": path, 
+                                "headers": {
+                                    "Content-Type": "application/x-www-form-urlencoded"
+                                }
+                            }, function (imgFlipRes) {
+                                var imgChunks = [];
+                                imgFlipRes.on("data", function (chunk) {
+                                    imgChunks.push(chunk);
+                                });
+                                imgFlipRes.on("end", function () {
+                                    var imgFlipBody = Buffer.concat(imgChunks);
+                                    var imgFlipObj = JSON.parse(imgFlipBody.toString());
+                                    console.log(imgFlipObj);
+                                    msg.channel.send(imgFlipObj.data.url);
+                                });
+                            });
+                            imgFlip.write(JSON.stringify({}));
+                            imgFlip.end();
+                            
+                        });
+                    });
 
-                    var options = {
-                      "method": "GET",
-                      "hostname": "api.chucknorris.io",
-                      "port": null,
-                      "path": "/jokes/random"
-                    };
-                    
-                    var req = http.request(options, function (res) {
-                      var chunks = [];
-                    
-                      res.on("data", function (chunk) {
-                        chunks.push(chunk);
-                      });
-                    
-                      res.on("end", function () {
-                        var body = Buffer.concat(chunks);
-                        console.log(body.toString());
-                        var responseObj = JSON.parse(body.toString());
-                        msg.channel.send(responseObj.value);
-                      });
-                    });
-                    
-                    req.end();
-                }
-            });
-        }
-    },
-    help: '`!chuck` he doesn\'t sleep, he waits....'
+                    req.end();
+                }
+            });
+        }
+    },
+    help: '`!chuck` he doesn\'t sleep, he waits....'
 };
 
