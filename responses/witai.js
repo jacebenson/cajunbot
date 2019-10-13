8a3392d392fcf563ca5b899ab48f87521cf30e4a
Jace Benson <jace@protonmail.com>
2019-10-12 19:01:37 -0500

    removed dm logic

--- responses/witai.js
+++ responses/witai.js
@@ -23,64 +23,59 @@ module.exports = {
           }
           return bool;
         })();
-        if (mentionsCajunBot) {
+        if (mentionsCajunBot || msg.channel.type === 'dm') {
           //only send messages to wit.ai if to cajunbot
           console.log('message is for cajunbot');
           client.message(msg.content, {})
             .then((data) => {
               console.log('yay, got wit.ai response: ', JSON.stringify(data, '', ' '));
-              if (msg.channel.type === 'dm') {
-                console.log('message is for bot');
-                  for (var prop in data.entities) {
-                    if (prop === "intent") {
-                      data.entities[prop].forEach(function (intent) {
-                        console.log(intent);
-                        if (intent.value === 'dad-joke' && intent.confidence > 0.9) {
-                          responses['joke.js']._command(msg, 1);
-                        }
-                        if (intent.value === 'quote' && intent.confidence > 0.9) {
-                          if (data.entities.contact) {
-                            var mentionsChuck = data.entities.contact.filter(function (contact) {
-                              if (contact.value.toLowerCase() === 'chuck') {
-                                return true;
-                              }
-                              if (contact.value.toLowerCase() === 'norris') {
-                                return true;
-                              }
-                              if (contact.value.toLowerCase() === 'chuck norris') {
-                                return true;
-                              }
-                            });
-                            var mentionsRon = data.entities.contact.filter(function (contact) {
-                              if (contact.value.toLowerCase() === 'ron') {
-                                return true;
-                              }
-                              if (contact.value.toLowerCase() === 'swanson') {
-                                return true;
-                              }
-                              if (contact.value.toLowerCase() === 'ron swanson') {
-                                return true;
-                              }
-                            });
-                            if (mentionsChuck.length > 0) {
-                              console.log('chuck meme incoming');
-                              responses['chuck-norris.js']._command(msg);
-                            } else if (mentionsRon.length > 0) {
-                              console.log('ron meme incoming');
-                              responses['ron-swanson.js']._command(msg);
-                            } else {
-                              console.log('quote about who? ', data.entities.contact[0].value);
-                            }
-
-                          } else {
-                            console.log('tell me a quote...', JSON.stringify(data, '', ' '));
-                          }
-                        }
-                      });
+              for (var prop in data.entities) {
+                if (prop === "intent") {
+                  data.entities[prop].forEach(function (intent) {
+                    console.log(intent);
+                    if (intent.value === 'dad-joke' && intent.confidence > 0.9) {
+                      responses['joke.js']._command(msg, 1);
                     }
-                  }
-              } else {
-                console.log('not a dm msg.channel.type: ', msg.channel.type);
+                    if (intent.value === 'quote' && intent.confidence > 0.9) {
+                      if (data.entities.contact) {
+                        var mentionsChuck = data.entities.contact.filter(function (contact) {
+                          if (contact.value.toLowerCase() === 'chuck') {
+                            return true;
+                          }
+                          if (contact.value.toLowerCase() === 'norris') {
+                            return true;
+                          }
+                          if (contact.value.toLowerCase() === 'chuck norris') {
+                            return true;
+                          }
+                        });
+                        var mentionsRon = data.entities.contact.filter(function (contact) {
+                          if (contact.value.toLowerCase() === 'ron') {
+                            return true;
+                          }
+                          if (contact.value.toLowerCase() === 'swanson') {
+                            return true;
+                          }
+                          if (contact.value.toLowerCase() === 'ron swanson') {
+                            return true;
+                          }
+                        });
+                        if (mentionsChuck.length > 0) {
+                          console.log('chuck meme incoming');
+                          responses['chuck-norris.js']._command(msg);
+                        } else if (mentionsRon.length > 0) {
+                          console.log('ron meme incoming');
+                          responses['ron-swanson.js']._command(msg);
+                        } else {
+                          console.log('quote about who? ', data.entities.contact[0].value);
+                        }
+
+                      } else {
+                        console.log('tell me a quote...', JSON.stringify(data, '', ' '));
+                      }
+                    }
+                  });
+                }
               }
             })
         } else {
